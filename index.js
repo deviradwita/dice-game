0529cd92b14826e1af8b4410844e33ff2eb06b38
deviradwita <104553733+deviradwita@users.noreply.github.com>
2022-10-26 16:05:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector(".btn").addEventListener("click", function() {
+
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var img1 = `images/dice${randomNumber1}.png`;
+  document.querySelector(".img1").setAttribute("src", img1);
+
+  var img2 = `images/dice${randomNumber2}.png`;
+  document.querySelector(".img2").setAttribute("src", img2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+  } else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw👏"
+  };
+
+
+})
